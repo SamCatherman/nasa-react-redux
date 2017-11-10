ffa4707e41fa743f3212bc26557012eca81041e9
SamCatherman <sam.catherman@gmail.com>
2017-11-09 20:36:21 -0500

    adds redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import './assets/stylesheets/styles.scss';
-
 import React from 'react'; // import the main react dependency
 import ReactDOM from 'react-dom'; // import reactDOM
 import { Provider } from 'react-redux';
+import Store from './Store'
 import App from './app/App'; // import the main app component
 
+import './assets/stylesheets/styles.scss';
+
 ReactDOM.render(
-  <Provider>
+  <Provider store={Store}> 
     <App />
   </Provider>,
   document.getElementById('root')
---
+++ src/reducers/app_reducer.js
@@ -0,0 +1,5 @@
+export default (state = {}, action) => {
+//two arguments, initial state set to an empty object, and an action
+// reducers always return state
+  return state
+}
